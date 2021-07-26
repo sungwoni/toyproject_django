e762eee8887c129ea3f49a7197ad9cc644ee6b81
sungwoni <tjddnjs2013@gmail.com>
2021-07-26 23:03:42 +0900

    Initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+app_name = 'accountapp'  #앞으로 함수호출할 때 더 간단해지니까 해보기
+
+urlpatterns=[
+    path('hello_world/', hello_world, name='hello_world')
+]
